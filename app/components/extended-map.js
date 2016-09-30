73f42ca9eb5a48d9e359f1825abc9ef670c17f0f
David Jackson <altmail@jackson-wallace.net>
2016-09-30 11:50:34 -0500

    Simplified again.

--- app/components/extended-map.js
+++ app/components/extended-map.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import LeafletMap from 'ember-leaflet/components/leaflet-map';
 
 export default LeafletMap.extend({
+  value: 0,
   didCreateLayer() {
     this._super(...arguments);
 
