5c2057078ca46789fda25c0551d0ed8419c19ea1
David Jackson <altmail@jackson-wallace.net>
2016-09-30 11:49:50 -0500

    Further simplified repro.

--- app/components/extended-map.js
+++ app/components/extended-map.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 import LeafletMap from 'ember-leaflet/components/leaflet-map';
 
-import TrackZoomAndBoundsMixin from 'leaflet-bug-repro/mixins/track-zoom-and-bounds';
-
-export default LeafletMap.extend(TrackZoomAndBoundsMixin, {
+export default LeafletMap.extend({
   didCreateLayer() {
     this._super(...arguments);
 
